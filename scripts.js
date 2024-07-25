445dacbf3436103775e44fcc941e253f340b43d9
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-07-25 15:22:02 -0700

    Add remove book feature

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
 "use strict";
+function generateUUID() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+}
+
 function Library() {
     this.books = [];
 } 
 Library.prototype.addBook = function(book) {
     this.books.push(book);
 }
+Library.prototype.removeBookByUUID = function(bookUUID) {
+    this.books = this.books.filter((book) => book.uuid !== bookUUID);
+}
 const myLibrary = new Library();
 
 // Book Constructor
-function Book(title, author, pages, hasRead) {
+function Book(uuid, title, author, pages, hasRead) {
+    this.uuid = uuid;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -22,16 +32,23 @@ function createBookCard(book) {
     const bookPages = document.createElement('p');
     const bookHasRead = document.createElement('input');
     bookHasRead.setAttribute("type", "checkbox");
+    const removeBookBtn = document.createElement('button');
+    removeBookBtn.textContent = 'Remove';
 
     bookTitle.textContent = book.title;
     bookAuthor.textContent = book.author;
     bookPages.textContent = book.pages;
     bookHasRead.checked = book.hasRead;
+    removeBookBtn.addEventListener("click", () => {
+        myLibrary.removeBookByUUID(book.uuid);
+        updateBookDisplay();
+    });
 
     bookContainer.appendChild(bookTitle);
     bookContainer.appendChild(bookAuthor);
     bookContainer.appendChild(bookPages);
     bookContainer.appendChild(bookHasRead);
+    bookContainer.appendChild(removeBookBtn);
 
     return bookContainer;
 }
@@ -54,12 +71,12 @@ addBookBtn.addEventListener("click", () => {
 addBookForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const bookData = Object.fromEntries(new FormData(addBookForm));
-    const book = new Book(bookData.bookTitle, bookData.bookAuthor, bookData.bookPages, "bookHasRead" in bookData ? true : false);
+    const book = new Book(generateUUID(), bookData.bookTitle, bookData.bookAuthor, bookData.bookPages, "bookHasRead" in bookData ? true : false);
     myLibrary.addBook(book);
     updateBookDisplay();
     addBookModal.close();
     addBookForm.reset();
-    
+    console.log(myLibrary.books);
 });
 closeBookModalBtn.addEventListener("click", () => {
     addBookForm.reset();
