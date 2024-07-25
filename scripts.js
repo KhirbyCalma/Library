b9396938f1a11056647ac33bc231520f17b958d9
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-07-25 11:16:25 -0700

    Add basic library addition and display

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 "use strict";
 
 console.log("Hello world!");
+
+const myLibrary = [];
+
+// Book Constructor
+function Book(title) {
+    this.title = title;
+}
+
+// Adds book object to library array
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+for (let i = 0; i < 3; i++) {
+    let bookTitle = prompt("Input title of a book:", "Example Title");
+    addBookToLibrary(new Book(bookTitle));
+}
+console.table(myLibrary);
