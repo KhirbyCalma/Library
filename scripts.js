cde02acaedb2165fb5fc199e638fca166fd9058a
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-07-25 11:41:18 -0700

    Add more properties to book object

--- scripts.js
+++ scripts.js
@@ -5,8 +5,11 @@ console.log("Hello world!");
 const myLibrary = [];
 
 // Book Constructor
-function Book(title) {
+function Book(title, author, pages, hasRead) {
     this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = this.hasRead;
 }
 
 // Adds book object to library array
