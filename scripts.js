392975f964904d9b1e09a42ba2b6c53f8b1b5a83
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-07-25 16:09:13 -0700

    Reorder and comment js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,40 @@
 "use strict";
+// QUERY SELECTORS
+const booksContainer = document.querySelector('main .books');
+const addBookBtn = document.getElementById("add-book");
+const addBookModal = document.getElementById("add-book-modal");
+const addBookForm = document.getElementById("book-form");
+const closeBookModalBtn = document.getElementById("close-book-modal");
+
+// EVENT LISTENERS
+addBookBtn.addEventListener("click", () => {
+    addBookModal.showModal();
+});
+
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const bookData = Object.fromEntries(new FormData(addBookForm));
+    const book = new Book(generateUUID(), bookData.bookTitle, bookData.bookAuthor, bookData.bookPages, "bookHasRead" in bookData ? true : false);
+    myLibrary.addBook(book);
+    updateBookDisplay();
+    addBookModal.close();
+    addBookForm.reset();
+    console.log(myLibrary.books);
+});
+
+closeBookModalBtn.addEventListener("click", () => {
+    addBookForm.reset();
+    addBookModal.close();
+});
+
+// CUSTOM FUNCTIONS
 function generateUUID() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
         (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
     );
 }
 
-function Library() {
-    this.books = [];
-} 
-Library.prototype.addBook = function(book) {
-    this.books.push(book);
-}
-Library.prototype.removeBookByUUID = function(bookUUID) {
-    this.books = this.books.filter((book) => book.uuid !== bookUUID);
-}
-const myLibrary = new Library();
-
-// Book Constructor
-function Book(uuid, title, author, pages, hasRead) {
-    this.uuid = uuid;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
-}
-
+// FUNCTION TO CREATE BOOK CARD HTML ELEMENT
 function createBookCard(book) {
     const bookContainer = document.createElement('div');
     const bookTitle = document.createElement('h1');
@@ -53,6 +63,7 @@ function createBookCard(book) {
     return bookContainer;
 }
 
+// FUNCTION TO UPDATE BOOK DISPLAY WHENEVER CHANGES MADE TO LIBRARY'S BOOKS
 function updateBookDisplay() {
     booksContainer.innerHTML = '';
     for (let book of myLibrary.books) {
@@ -60,25 +71,24 @@ function updateBookDisplay() {
     }
 }
 
-const booksContainer = document.querySelector('main .books');
-const addBookBtn = document.getElementById("add-book");
-const addBookModal = document.getElementById("add-book-modal");
-const addBookForm = document.getElementById("book-form");
-const closeBookModalBtn = document.getElementById("close-book-modal");
-addBookBtn.addEventListener("click", () => {
-    addBookModal.showModal();
-});
-addBookForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const bookData = Object.fromEntries(new FormData(addBookForm));
-    const book = new Book(generateUUID(), bookData.bookTitle, bookData.bookAuthor, bookData.bookPages, "bookHasRead" in bookData ? true : false);
-    myLibrary.addBook(book);
-    updateBookDisplay();
-    addBookModal.close();
-    addBookForm.reset();
-    console.log(myLibrary.books);
-});
-closeBookModalBtn.addEventListener("click", () => {
-    addBookForm.reset();
-    addBookModal.close();
-});
+// CUSTOM OBJECTS
+// LIBRARY OBJECT (ATTRIBUTES AND METHODS)
+function Library() {
+    this.books = [];
+} 
+Library.prototype.addBook = function(book) {
+    this.books.push(book);
+}
+Library.prototype.removeBookByUUID = function(bookUUID) {
+    this.books = this.books.filter((book) => book.uuid !== bookUUID);
+}
+const myLibrary = new Library();
+
+// BOOK OBJECT (ATTRIBUTES)
+function Book(uuid, title, author, pages, hasRead) {
+    this.uuid = uuid;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
