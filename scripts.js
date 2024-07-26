a276658904f2006e6ceb3783c30ac55fcc9e5393
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-07-26 13:55:59 -0700

    Style book card

--- scripts.js
+++ scripts.js
@@ -30,20 +30,29 @@ closeBookModalBtn.addEventListener("click", () => {
 // FUNCTION TO CREATE BOOK CARD HTML ELEMENT
 function createBookCard(book) {
     const bookContainer = document.createElement('div');
+    bookContainer.classList.add("book");
     const bookTitle = document.createElement('h1');
     const bookAuthor = document.createElement('h2');
     const bookPages = document.createElement('p');
-    const bookHasRead = document.createElement('input');
-    bookHasRead.setAttribute("type", "checkbox");
+    const bookHasReadContainer = document.createElement('div');
+    const bookHasReadLabel = document.createElement('label');
+    const bookHasReadCheckbox = document.createElement('input');
+    bookHasReadLabel.setAttribute("for", "book-has-read-checkbox");
+    bookHasReadCheckbox.setAttribute("id", "book-has-read-checkbox");
+    bookHasReadCheckbox.setAttribute("type", "checkbox");
+    bookHasReadCheckbox.setAttribute("name", "bookHasRead");
+    bookHasReadContainer.appendChild(bookHasReadLabel);
+    bookHasReadContainer.appendChild(bookHasReadCheckbox)
     const removeBookBtn = document.createElement('button');
     removeBookBtn.textContent = 'Remove';
 
-    bookTitle.textContent = book.title;
+    bookTitle.textContent = `"${book.title}"`;
     bookAuthor.textContent = book.author;
-    bookPages.textContent = book.pages;
-    bookHasRead.checked = book.hasRead;
+    bookPages.textContent = `${book.pages} pages`;
+    bookHasReadLabel.textContent = "Read?";
+    bookHasReadCheckbox.checked = book.hasRead;
 
-    bookHasRead.addEventListener("click", () => {
+    bookHasReadCheckbox.addEventListener("click", () => {
         myLibrary.updateBookReadStatus(book);
         updateBookDisplay();
     });
@@ -55,7 +64,7 @@ function createBookCard(book) {
     bookContainer.appendChild(bookTitle);
     bookContainer.appendChild(bookAuthor);
     bookContainer.appendChild(bookPages);
-    bookContainer.appendChild(bookHasRead);
+    bookContainer.appendChild(bookHasReadContainer);
     bookContainer.appendChild(removeBookBtn);
 
     return bookContainer;
@@ -76,12 +85,15 @@ function Library() {
 } 
 Library.prototype.addBook = function(book) {
     this.books.push(book);
+    console.table(this.books);
 }
 Library.prototype.removeBook = function(bookToBeRemoved) {
     this.books = this.books.filter((book) => book !== bookToBeRemoved);
+    console.table(this.books);
 }
 Library.prototype.updateBookReadStatus = function(bookToBeUpdatedReadStatus) {
     bookToBeUpdatedReadStatus.hasRead = !bookToBeUpdatedReadStatus.hasRead;
+    console.table(this.books);
 }
 const myLibrary = new Library();
 
